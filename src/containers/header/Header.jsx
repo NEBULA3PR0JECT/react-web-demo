60b411f15f173d7c2a0aafa1f0468d67bccc215a
Ilan <bebetteryou2015@gmail.com>
2023-02-04 19:34:40 +0200

    files for local server

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
 
   useEffect(() => {
     if (urlLink !== ai) {
-      fetch('https://anarhist.pythonanywhere.com/insert_pipeline_id', {
+      fetch('http://localhost:5000/insert_pipeline_id', {
         method: 'POST',
         body: JSON.stringify(urlLink),
         headers: { 'content-type': 'application/json' },
