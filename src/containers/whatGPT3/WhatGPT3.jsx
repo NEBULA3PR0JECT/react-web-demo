fee956571d38db83387b4e0c789ca0618e8f018d
Ilan <bebetteryou2015@gmail.com>
2023-02-10 20:38:44 +0200

    fixed styling

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 // import Feature from '../../components/feature/Feature';
-import Graph from 'react-graph-vis';
+// import Graph from 'react-graph-vis';
 import './whatGPT3.css';
 import ai from '../../assets/ai.png';
 
-const options = {
-  layout: {
-    hierarchical: false,
-  },
-  edges: {
-    color: '#000000',
-  },
-};
+// const options = {
+//   layout: {
+//     hierarchical: false,
+//   },
+//   edges: {
+//     color: '#000000',
+//   },
+// };
 
-function randomColor() {
-  const red = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-  const green = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-  const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-  return `#${red}${green}${blue}`;
-}
+// function randomColor() {
+//   const red = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+//   const green = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+//   const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+//   return `#${red}${green}${blue}`;
+// }
 
 const WhatGPT3 = ({ recPipelineId, isFinished }) => {
   console.log(`Recieved: whatgpt3 ${recPipelineId}`);
@@ -28,34 +28,34 @@ const WhatGPT3 = ({ recPipelineId, isFinished }) => {
   const [generatedTriplets, setGeneratedTriplets] = useState([]);
   const [pipelineId, setPipelineId] = useState(false);
 
-  const [graphState, setGraphState] = useState({
-    counter: 0,
-    tripletidx: 0,
-    graph: {
-      nodes: [
-        // { id: 1, label: 'person', color: '#e04141' },
-        // { id: 2, label: 'has', color: '#e09c41' },
-        // { id: 3, label: 'bike', color: '#e0df41' },
-      ],
-      edges: [
-        // { from: 1, to: 2 },
-        // { from: 2, to: 3 },
-      ],
-    },
-    events: {
-      select: ({ nodes, edges }) => {
-        console.log('Selected nodes:');
-        console.log(nodes);
-        console.log('Selected edges:');
-        console.log(edges);
-        alert(`Selected node: ${nodes}`);
-      },
-      doubleClick: () => {
-        // createNode();
-      },
-    },
-  });
-  const { graph, events } = graphState;
+  // const [graphState, setGraphState] = useState({
+  //   counter: 0,
+  //   tripletidx: 0,
+  //   graph: {
+  //     nodes: [
+  //       // { id: 1, label: 'person', color: '#e04141' },
+  //       // { id: 2, label: 'has', color: '#e09c41' },
+  //       // { id: 3, label: 'bike', color: '#e0df41' },
+  //     ],
+  //     edges: [
+  //       // { from: 1, to: 2 },
+  //       // { from: 2, to: 3 },
+  //     ],
+  //   },
+  //   events: {
+  //     select: ({ nodes, edges }) => {
+  //       console.log('Selected nodes:');
+  //       console.log(nodes);
+  //       console.log('Selected edges:');
+  //       console.log(edges);
+  //       alert(`Selected node: ${nodes}`);
+  //     },
+  //     doubleClick: () => {
+  //       // createNode();
+  //     },
+  //   },
+  // });
+  // const { graph, events } = graphState;
   // const [isFetching, setIsFetching] = useState(true);
   // const [isFetching, setIsFetching] = useState(true);
   // const [movieId, setMovieId] = useState(false);
@@ -117,43 +117,43 @@ const WhatGPT3 = ({ recPipelineId, isFinished }) => {
     }, []);
   }
 
-  const createNode = (generatedTripletsThrees) => {
-    const color = randomColor();
-    setGraphState(({ graph: { nodes, edges }, counter, tripletidx, ...rest }) => {
-      const id = counter + 1;
-      const secondId = counter + 2;
-      const thirdId = counter + 3;
-      console.log(`counter: ${counter}`);
-      console.log(`tripletIdx: ${tripletidx}`);
-      // const from = Math.floor(Math.random() * (counter - 1)) + 1;
-      return {
-        ...rest,
-        graph: {
-          nodes: [
-            ...nodes,
-            { id: `${id}`, label: `${generatedTripletsThrees[tripletidx][0]}`, color },
-            { id: `${secondId}`, label: `${generatedTripletsThrees[tripletidx][1]}`, color },
-            { id: `${thirdId}`, label: `${generatedTripletsThrees[tripletidx][2]}`, color },
-          ],
-          edges: [
-            ...edges,
-            { from: id, to: secondId },
-            { from: secondId, to: thirdId },
-          ],
-        },
-        counter: thirdId,
-        tripletidx: tripletidx + 1,
-      };
-    });
-  };
+  // const createNode = (generatedTripletsThrees) => {
+  //   const color = randomColor();
+  //   setGraphState(({ graph: { nodes, edges }, counter, tripletidx, ...rest }) => {
+  //     const id = counter + 1;
+  //     const secondId = counter + 2;
+  //     const thirdId = counter + 3;
+  //     console.log(`counter: ${counter}`);
+  //     console.log(`tripletIdx: ${tripletidx}`);
+  //     // const from = Math.floor(Math.random() * (counter - 1)) + 1;
+  //     return {
+  //       ...rest,
+  //       graph: {
+  //         nodes: [
+  //           ...nodes,
+  //           { id: `${id}`, label: `${generatedTripletsThrees[tripletidx][0]}`, color },
+  //           { id: `${secondId}`, label: `${generatedTripletsThrees[tripletidx][1]}`, color },
+  //           { id: `${thirdId}`, label: `${generatedTripletsThrees[tripletidx][2]}`, color },
+  //         ],
+  //         edges: [
+  //           ...edges,
+  //           { from: id, to: secondId },
+  //           { from: secondId, to: thirdId },
+  //         ],
+  //       },
+  //       counter: thirdId,
+  //       tripletidx: tripletidx + 1,
+  //     };
+  //   });
+  // };
 
-  if (isFinished) {
-    const generatedTripletsThrees = generatedTriplets.filter((element) => element.length === 3);
-    console.log(generatedTripletsThrees);
-    generatedTripletsThrees.forEach(() => {
-      createNode(generatedTripletsThrees);
-    });
-  }
+  // if (isFinished) {
+  //   const generatedTripletsThrees = generatedTriplets.filter((element) => element.length === 3);
+  //   console.log(generatedTripletsThrees);
+  //   generatedTripletsThrees.forEach(() => {
+  //     createNode(generatedTripletsThrees);
+  //   });
+  // }
   // useEffect(() => {
   //   const interval = setInterval(() => {
   //     fetch('http://74.82.29.209:5000/get_fetching_status', {
@@ -196,7 +196,7 @@ const WhatGPT3 = ({ recPipelineId, isFinished }) => {
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">Generated triplets:</h1>
       </div>
-      <div className="gpt3__whatgpt3-heading">
+      <div className="gpt3__whatgpt3-heading-triplet">
         {updatedList}
         {/* <Graph graph={graph} options={options} events={events} style={{ height: '640px' }} /> */}
       </div>
